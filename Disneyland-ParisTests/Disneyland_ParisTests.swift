eeb2e0512b82fea00b042803e222cfdd2d9fbd74
Thomas Durand <thomas.durand.verjat@gmail.com>
2015-06-15 11:33:27 -0400

    Project init with Xcode 7

---
+++ Disneyland-ParisTests/Disneyland_ParisTests.swift
@@ -0,0 +1,35 @@
+//
+//  Disneyland_ParisTests.swift
+//  Disneyland-ParisTests
+//
+//  Created by Thomas Durand on 15/06/2015.
+//  Copyright © 2015 Dean. All rights reserved.
+//
+
+import XCTest
+
+class Disneyland_ParisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
